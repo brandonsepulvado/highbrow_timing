5a33d409d1264407a211d3b2dd9173bbd4cc1733
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2020-03-08 21:13:10 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# highbrow_timing
+# Timing of Highbrow Cultural Socialization and the Conversion to Economic Capital
+
+## Authors
+
+* Brandon Sepulvado, _NORC at the University of Chicago_
+* Omar Lizardo, _UCLA_
+* Michael Lee Wood, _BYU_
+
+## Directory Structure
+
+The files in this repository use relative paths, so it might be helpful to review the directory strucure. In the primary highbrow_timing directory, you will find the R project file alongside code, data, and output folders (in addition to the .gitignore and README.md files). The code folder contains all of code to reproduce the analyses and data cleaning/manipulation used in this paper, and the output folder contains the output file (e.g., regression tables and visualizations). The data folder contains the 1973 Americans and the Arts survey way. We obtained the files from [ICPSR](). The survey data is provided from the site in .rda format, and we simply converted that file to .rds format. The code folder only needs to contain the .rds file
---
+++ code/data_load_prep.R
@@ -0,0 +1,48 @@
+# ==============================================================================
+# americans and the arts 
+# ==============================================================================
+
+# load packages
+library(dplyr)
+library(here)
+library(prettyR)
+library(glue)
+library(purrr)
+library(stringr)
+
+# 1973 =========================================================================
+
+# load data
+data_1973 <- readRDS(here('data', 'aa_1973.rds'))
+
+# provide substantive names to variables of interest
+data_1973 <- data_1973 %>% 
+  rename(race = F11,
+         sex = F12,
+         age_group = F8,
+         edu = F5,
+         income = F9,
+         region = S11,
+         awf_play = Q24B_1,
+         awf_artmuseum = Q24B_2,
+         awf_concert = Q24B_3,
+         awf_opera = Q24B_4,
+         awf_scimuseum = Q24B_5,
+         awf_histsite = Q24B_6,
+         awf_ballet = Q24B_7)
+
+# change factors to numeric for additive scales
+
+# get vector of variables to recode
+vars_to_recode <- c(glue("Q5C_{1:7}"),
+                    glue("Q5D_{1:7}"),
+                    glue("Q5E_{1:7}"))
+
+# recode
+data_1973 <- data_1973 %>% 
+  mutate_at(vars_to_recode, ~case_when(is.na(.) ~ NA,
+                                       str_detect(., '0') ~ FALSE,
+                                       TRUE ~ TRUE)) %>% 
+  mutate(gs_hb = rowSums(select(., Q5C_1:Q5C_7), na.rm = TRUE),
+         jhs_hb = rowSums(select(., Q5D_1:Q5D_7), na.rm = TRUE),
+         col_hb = rowSums(select(., Q5E_1:Q5E_7), na.rm = TRUE))
