2ad8ec9507dc5785ea926d1fc0d446b0dce424c1
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2020-07-18 21:07:52 -0400

    Adding univariate descriptives; resolves #4

--- code/analysis.R
+++ code/analysis.R
@@ -6,13 +6,72 @@
 library(here)
 library(texreg)
 library(ggplot2)
+library(tidyr)
+library(ggalluvial)
+library(openxlsx)
+library(glue)
 
 # load data
 source(here('code', 'data_load_prep.R'))
 
 # descriptives =================================================================
 
+# income : unweighted counts
+data_1973 %>% 
+  filter(!is.na(income)) %>% 
+  #count(income) %>% 
+  ggplot(aes(x = income)) +
+  geom_bar(fill = 'navyblue', alpha = .6) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 50, hjust = 1)) +
+  labs(x = 'Income',
+       y = 'Number of Respondents')
 
+# function to get counts
+get_counts <- function(variable, data = data_1973){
+  # get counts
+  counts <- data %>% 
+    filter(!is.na({{variable}})) %>% 
+    count({{variable}})
+  # return counts
+  return(counts)
+}
+
+# counts : income
+counts_income <- get_counts(income)
+
+# race : unweighted counts
+counts_race <- get_counts(race)
+
+# gender : unweighted
+counts_gender <- get_counts(sex)
+
+# age
+counts_age <- get_counts(age_group)
+
+# education
+counts_edu <- get_counts(edu)
+
+# highbrow scales
+counts_gs_hb <- get_counts(gs_hb)
+counts_jhs_hb <- get_counts(jhs_hb)
+counts_col_hb <- get_counts(col_hb)
+
+# occupation
+counts_occ <- get_counts(occupation)
+
+# save counts
+write.xlsx(list(counts_income, counts_race, counts_gender, counts_age, 
+                counts_edu, counts_gs_hb, counts_jhs_hb, counts_col_hb),
+           sheetName= c("income", "race", "gender", "age", 
+                        "edu", "gs_hb", "jhs_hb", "col_hb"),
+           file = here('output', glue("univariate_counts_{Sys.Date()}.xlsx")))
+
+# changes in highbrow consumption
+# data_1973 %>% 
+#   select(CASEID, gs_hb, jhs_hb, col_hb) %>% 
+  
+  
 
 # regression models ============================================================
 
@@ -85,24 +144,57 @@ model_13 <- lm(as.numeric(income)~ race + sex + age_group + edu + gs_hb + jhs_hb
 summary(model_13)
 # occupational group for head of household changes things a lot 
 
+# create gender-based subsets
+
+# male
+data_1973_male <- data_1973 %>% filter(sex == "(1) Male")
+
+# female
+data_1973_female <- data_1973 %>% filter(sex == "(2) Female")
+
+
+# model 12 gender-based models (unconstrained)
+
+# male
+model_12_male <- lm(as.numeric(income) ~ race + age_group + edu + gs_hb + jhs_hb
+                    + col_hb, 
+                    data = data_1973_male,
+                    weights = data_1973_male$WEIGHT)
+
+# female
+model_12_female <- lm(as.numeric(income) ~ race + age_group + edu + gs_hb + jhs_hb
+                      + col_hb, 
+                      data = data_1973_female,
+                      weights = data_1973_female$WEIGHT)
+
+# model 13 gender-based (unconstrained) : male
+model_13_male <- lm(as.numeric(income) ~ race + age_group + edu + gs_hb + jhs_hb
+                    + col_hb + as.factor(occupation), 
+                    data = data_1973_male,
+                    weights = data_1973_male$WEIGHT)
+
+# model 13 gender-based (unconstrained) : female
+model_13_female <- lm(as.numeric(income) ~ race + age_group + edu + gs_hb + jhs_hb
+                      + col_hb + as.factor(occupation), 
+                      data = data_1973_female,
+                      weights = data_1973_female$WEIGHT)
+
 
 
 # diagnostics ==================================================================
 
-# unweighted counts
+# function to plot residuals
+plot_resid <- function(model, model_name = NULL){
+  # get plot to return
+  to_return <- qqnorm(resid({{model}}))
+  to_return <- qqline(resid({{model}}))
+  to_return <- mtext(model_name)
+  # return object
+  return(to_return)
+}
 
-# income : unweighted counts
-data_1973 %>% 
-  filter(!is.na(income)) %>% 
-  #count(income) %>% 
-  ggplot(aes(x = income)) +
-  geom_bar(fill = 'navyblue', alpha = .6) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 50, hjust = 1)) +
-  labs(x = 'Income',
-       y = 'Number of Respondents')
-
-# unweighted counts
+# model c
+plot_resid(model_c, model_name = 'Model 0')
 
 
 # output =======================================================================
